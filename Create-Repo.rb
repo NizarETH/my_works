bb87fda69914517d1183330cce0d1365d3c6dbf6
ivanove <ettaheri.nizar@gmail.com>
2015-08-19 09:33:06 +0100

    commit 1

--- Create-Repo.rb
+++ Create-Repo.rb
@@ -70,7 +70,7 @@ class Gitcreate
         msg=STDIN.gets.chomp()
         cmd3="git commit -m \"#{msg}\""
         system(cmd3)
-        #I add group-form-1 for the user
+        
         cmd4="git remote add origin git@#{@host}:#{@user}/#{name}.git"
         puts cmd4
         system(cmd4)
